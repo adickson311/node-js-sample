2e76e3ade044fa14b50ea8373862daf1f87bc116
Andre Dickson <andre.dickson@pnc.com>
2017-04-25 15:19:02 -0400

    building

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 var express = require('express')
 var app = express()
+var phantomJsCloud = require("phantomjscloud");
+var browser = new phantomJsCloud.BrowserApi({
+  apiKey: 'ak-z4j5n-1f2vp-0yrzv-cgqqv-33516'
+});
 
-app.set('port', (process.env.PORT || 5000))
+app.set('port', (process.env.PORT || 5009))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
@@ -11,3 +15,11 @@ app.get('/', function(request, response) {
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
+
+console.log(browser)
+
+browser.requestSingle({ url: "http://www.google.com", renderType: "plainText" }).then(function(response){
+    console.log(JSON.stringify(response.content));
+}).catch(function(er){
+    console.log(er);
+});
